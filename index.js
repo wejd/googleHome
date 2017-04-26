62666993f4f4f45ac6fd0e5cd68e76fbd01a29e7
wejd <wejd.daghfous@esprit.tn>
2017-04-26 11:04:31 +0100

    add yes intetn1212121212

--- index.js
+++ index.js
@@ -335,6 +335,34 @@ restService.post('/webhook', function(req, res) {
                         })
                 })
 
+            } else {
+                request.get({ url: 'http://vps341573.ovh.net:5050', json: true }).then(function(result) {
+                    return request.post({
+                        url: 'http://vps341573.ovh.net:5050',
+                        form: { key: result.list[0] }
+                    }).then(
+                        function(body) {
+                            console.log(body)
+
+                            if (body == "found") {
+
+                                return res.json({
+                                    speech: result.list[0] + ' has been selected',
+                                    source: 'webhook-echo-one',
+
+                                });
+
+                            } else {
+                                return res.json({
+                                    speech: 'I was unable to select ' + result.list[0] + ' . Please try again later',
+                                    source: 'webhook-echo-one',
+
+                                });
+                            }
+                        })
+                })
+
+
             }
 
             break;
