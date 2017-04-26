a18b08af0cc7c729a421f629e6bc67c15a05d1ec
wejd <wejd.daghfous@esprit.tn>
2017-04-26 10:35:55 +0100

    add yes intetn121

--- index.js
+++ index.js
@@ -232,7 +232,8 @@ restService.post('/webhook', function(req, res) {
                 })
             break;
         case 'yes':
-            if (req.body.result.contexts.name = "search") {
+            console.log('*****', req.body.result.contexts.name)
+            if (req.body.result.contexts.name == "search") {
                 request.get({ url: 'http://vps341573.ovh.net:5050/getConnectedDevice', json: true }).then(function(nameSpeakerconnected) {
                     return request.get({ url: 'http://vps341573.ovh.net:5050', json: true }).then(function(result) {
                         if (result) {
@@ -307,7 +308,7 @@ restService.post('/webhook', function(req, res) {
 
                 })
             }
-            if (req.body.result.contexts.name = "link") {
+            if (req.body.result.contexts.name == "link") {
 
                 return request.post({ url: 'http://vps341573.ovh.net:5050', form: { key: req.body.result.parameters.any } }).then(
                     function(body) {
