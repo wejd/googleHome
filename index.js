c5b3f0ba357fea00a791d0c5045e15de9b75a04d
wejd <wejd.daghfous@esprit.tn>
2017-05-03 10:07:09 +0100

    add inteny no12

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ restService.post('/webhook', function(req, res) {
                 function(body) {
                     console.log(body)
 
-                    if (!body) {
+                    if (body = false) {
 
                         return res.json({
                             speech: 'No allplay device have been selected!',
